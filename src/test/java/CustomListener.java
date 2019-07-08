da34163485b1b2bbb58446a7acc68b9a1d502584
Evgeny Tkach <1eluzive1@gmail.com>
2019-07-08 14:41:12 +0400

    TestNG - all tasks

---
+++ src/test/java/CustomListener.java
@@ -0,0 +1,33 @@
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class CustomListener implements ITestListener {
+    public void onTestStart(ITestResult iTestResult) {
+        System.out.println("Test started!");
+    }
+
+    public void onTestSuccess(ITestResult iTestResult) {
+        System.out.println("Test Succes");
+    }
+
+    public void onTestFailure(ITestResult iTestResult) {
+        System.out.println("Test Failure");
+    }
+
+    public void onTestSkipped(ITestResult iTestResult) {
+        System.out.println("Test Skipped");
+    }
+
+    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+        System.out.println("Test Failed But Within Success Percentage");
+    }
+
+    public void onStart(ITestContext iTestContext) {
+        System.out.println("Start");
+    }
+
+    public void onFinish(ITestContext iTestContext) {
+        System.out.println("Finish");
+    }
+}
