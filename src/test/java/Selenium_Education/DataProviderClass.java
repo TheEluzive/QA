5fa2723a38439815d1814d000e715bee9b02ba60
Evgeny Tkach <1eluzive1@gmail.com>
2019-07-09 20:08:50 +0400

    selenium(check email(not working))

--- src/test/java/Selenium_Education/DataProviderClass.java
+++ src/test/java/Selenium_Education/DataProviderClass.java
@@ -4,19 +4,11 @@ import org.testng.annotations.DataProvider;
 
 public class DataProviderClass {
 
-    @DataProvider(name = "dataProviderEmail")
-    public Object[][] DataProvidereEmail() {
-        return new Object[][]{
-                {"12w543df2ghjwga235@gmail.com"},
-                {"12ww32vfw5432kl2a35@gmail.com"},
-                {"12ww2345wgdfsgfsdl1235@gmail.com"}
-        };
-    }
-
     @DataProvider(name = "dataProviderForRegistration")
     public Object[][] DataProviderForRegistration() {
         return  new Object[][]{
                 {
+                        "1gdfgjhggfdgfdjghasgfh5@gmail.com",//email
                         "Evgeny",//firstname
                         "Tkach", //lastname
                         "2345dfghqwerty", //password
@@ -39,6 +31,54 @@ public class DataProviderClass {
                         "Adress", //address alias
 
                 },
+                {
+                        "12gfdhgfdgfs7gh5@gmail.com",//email
+                        "Evgeny",//firstname
+                        "Tkach", //lastname
+                        "2345dfghqwerty", //password
+                        "24", //day
+                        "5", //month
+                        "1997", //year
+                        true, //newsletter radio
+                        true, //optin radio
+                        "", //firstname + ...
+                        "", //lastname + ...
+                        "Street street", //adress1
+                        "5",//adress2
+                        "Volgograd", //city
+                        "12345", //postcode
+                        "21", //country //only USA
+                        "10", //state 1-50
+                        "something", //Additional information
+                        "12-23-34", //home phone
+                        "89876543210", //mobile number
+                        "Adress", //address alias
+
+                },
+                {
+                        "12jg3hgfdhdgfhfdds666665@gmail.com",//email
+                        "Evgeny",//firstname
+                        "Tkach", //lastname
+                        "2345dfghqwerty", //password
+                        "24", //day
+                        "5", //month
+                        "1997", //year
+                        true, //newsletter radio
+                        true, //optin radio
+                        "", //firstname + ...
+                        "", //lastname + ...
+                        "Street street", //adress1
+                        "5",//adress2
+                        "Volgograd", //city
+                        "12345", //postcode
+                        "21", //country //only USA
+                        "10", //state 1-50
+                        "something", //Additional information
+                        "12-23-34", //home phone
+                        "89876543210", //mobile number
+                        "Adress", //address alias
+
+                }
 
         };
     }
--- src/test/java/Selenium_Education/Registration.java
+++ src/test/java/Selenium_Education/Registration.java
@@ -26,70 +26,109 @@ public class Registration {
         driver.findElement(By.xpath("//a[@class='login']")).click();
 
     }
-    public boolean result;
-    @Test(priority = 2, dataProvider = "dataProviderEmail", dataProviderClass = DataProviderClass.class)
-    public void inputEmail(String email) throws Exception  {
+    public boolean result=false;
 
-        WebElement inputEmail = (new WebDriverWait(driver, 10))
-                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='email_create']")));//or 2
-        inputEmail.clear();
-        //driver.findElement(By.xpath("//input[@id='email_create']")).clear();
-        driver.findElement(By.xpath("//input[@id='email_create']")).sendKeys(email);
-        driver.findElement(By.xpath("//form[@id='create-account_form']//span[1]")).click();
-        WebElement genderRadio = (new WebDriverWait(driver, 10))
-        .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='id_gender1']")));//or 2
-        genderRadio.click();
-
-
-        if (driver.findElements(By.xpath("//div[@id='create_account_error']")).isEmpty()) result=true; else {
-            result = false;
-            throw new Exception("This email used!");
-        }
-
-    }
 
 
 
     @Test(priority = 3, dataProvider = "dataProviderForRegistration", dataProviderClass = DataProviderClass.class)
-    public void inputPersonalInformation(String firstName, String lastName, String password, String day, String month,
+    public void inputPersonalInformation(String email,String firstName, String lastName, String password, String day, String month,
                                          String year, boolean newsletter, boolean optin, String firstNameAgain, String lastNameAgain,
                                          String adress1, String adress2, String city, String postcode, String country,
-                                         String state, String additional, String homePhone, String mobilePhone, String adressOptional){
-        if (result) {
-            driver.findElement(By.xpath("//input[@id='customer_firstname']")).sendKeys(firstName);
-            driver.findElement(By.xpath("//input[@id='customer_lastname']")).sendKeys(lastName);
-            driver.findElement(By.xpath("//input[@id='passwd']")).sendKeys(password);
-            Select selectDay = new Select(driver.findElement(By.xpath("//select[@id='days']")));
-            selectDay.selectByValue(day);
-            Select selectMonth = new Select(driver.findElement(By.xpath("//select[@id='months']")));
-            selectMonth.selectByValue(month);
-            Select selectYear = new Select(driver.findElement(By.xpath("//select[@id='years']")));
-            selectYear.selectByValue(year);
-            if (newsletter) driver.findElement(By.xpath("//input[@id='newsletter']")).click();
-            if (optin) driver.findElement(By.xpath("//input[@id='optin']")).click();
-            driver.findElement(By.xpath("//input[@id='firstname']")).sendKeys(firstNameAgain);
-            driver.findElement(By.xpath("//input[@id='lastname']")).sendKeys(lastNameAgain);
-            driver.findElement(By.xpath("//input[@id='address1']")).sendKeys(adress1);
-            driver.findElement(By.xpath("//input[@id='address2']")).sendKeys(adress2);
-            driver.findElement(By.xpath("//input[@id='city']")).sendKeys(city);
-            driver.findElement(By.xpath("//input[@id='postcode']")).sendKeys(postcode);
-            Select selectCountry = new Select(driver.findElement(By.xpath("//select[@id='id_country']")));
-            selectCountry.selectByValue(country); //only 21
-            Select selectState = new Select(driver.findElement(By.xpath("//select[@id='id_state']")));
-            selectState.selectByValue(state);//1-50
-            driver.findElement(By.xpath("//textarea[@id='other']")).sendKeys(additional);
-            driver.findElement(By.xpath("//input[@id='phone']")).sendKeys(homePhone);
-            driver.findElement(By.xpath("//input[@id='phone_mobile']")).sendKeys(mobilePhone);
-            driver.findElement(By.xpath("//input[@id='alias']")).clear();
-            driver.findElement(By.xpath("//input[@id='alias']")).sendKeys(adressOptional);
-            driver.findElement(By.xpath("//span[contains(text(),'Register')]")).click();
+                                         String state, String additional, String homePhone, String mobilePhone, String adressOptional) throws Exception {
+        WebElement inputEmail = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='email_create']")));//or 2
+        inputEmail.clear();
+
+        driver.findElement(By.xpath("//input[@id='email_create']")).sendKeys(email);
+        driver.findElement(By.xpath("//form[@id='create-account_form']//span[1]")).click();
 
 
-            driver.navigate().to(url);
-            WebElement buttonLogout = (new WebDriverWait(driver, 10))
-                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@class='logout']")));//or 2
-            buttonLogout.click();
-            openRegistration();
+
+        /*if (driver.findElements(By.xpath("//div[@id='create_account_error']")).isEmpty()) {
+            driver.navigate().refresh();
+            throw new Exception("This email used!");
+        }*/
+
+
+                if (driver.findElement(By.xpath("//div[@id='create_account_error']")).isDisplayed()){
+                    driver.navigate().refresh();
+                    driver.quit();
+                    throw  new Exception("This email is used");
+                }else {
+
+                    WebElement genderRadio = (new WebDriverWait(driver, 10))
+                            .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='id_gender1']")));//or 2
+                    genderRadio.click();
+                    driver.findElement(By.xpath("//input[@id='customer_firstname']")).sendKeys(firstName);
+                    driver.findElement(By.xpath("//input[@id='customer_lastname']")).sendKeys(lastName);
+                    driver.findElement(By.xpath("//input[@id='passwd']")).sendKeys(password);
+                    Select selectDay = new Select(driver.findElement(By.xpath("//select[@id='days']")));
+                    selectDay.selectByValue(day);
+                    Select selectMonth = new Select(driver.findElement(By.xpath("//select[@id='months']")));
+                    selectMonth.selectByValue(month);
+                    Select selectYear = new Select(driver.findElement(By.xpath("//select[@id='years']")));
+                    selectYear.selectByValue(year);
+                    if (newsletter) driver.findElement(By.xpath("//input[@id='newsletter']")).click();
+                    if (optin) driver.findElement(By.xpath("//input[@id='optin']")).click();
+                    driver.findElement(By.xpath("//input[@id='firstname']")).sendKeys(firstNameAgain);
+                    driver.findElement(By.xpath("//input[@id='lastname']")).sendKeys(lastNameAgain);
+                    driver.findElement(By.xpath("//input[@id='address1']")).sendKeys(adress1);
+                    driver.findElement(By.xpath("//input[@id='address2']")).sendKeys(adress2);
+                    driver.findElement(By.xpath("//input[@id='city']")).sendKeys(city);
+                    driver.findElement(By.xpath("//input[@id='postcode']")).sendKeys(postcode);
+                    Select selectCountry = new Select(driver.findElement(By.xpath("//select[@id='id_country']")));
+                    selectCountry.selectByValue(country); //only 21
+                    Select selectState = new Select(driver.findElement(By.xpath("//select[@id='id_state']")));
+                    selectState.selectByValue(state);//1-50
+                    driver.findElement(By.xpath("//textarea[@id='other']")).sendKeys(additional);
+                    driver.findElement(By.xpath("//input[@id='phone']")).sendKeys(homePhone);
+                    driver.findElement(By.xpath("//input[@id='phone_mobile']")).sendKeys(mobilePhone);
+                    driver.findElement(By.xpath("//input[@id='alias']")).clear();
+                    driver.findElement(By.xpath("//input[@id='alias']")).sendKeys(adressOptional);
+                    driver.findElement(By.xpath("//span[contains(text(),'Register')]")).click();
+
+
+                    driver.navigate().to(url);
+                    WebElement buttonLogout = (new WebDriverWait(driver, 10))
+                            .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@class='logout']")));//or 2
+                    buttonLogout.click();
+                    openRegistration();
+
+
+
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+       /* String saveEmail="" + email;
+        while (!result){
+            if (driver.findElement(By.xpath("//div[@id='create_account_error']")).isDisplayed()) result=true;
+            else {
+
+
+                driver.findElement(By.xpath("//input[@id='email_create']")).clear();
+                saveEmail=(int)(Math.random()*10+1)+saveEmail;
+               // email=(int)(Math.random()*10+1)+email;
+                System.out.println("Used email: " + saveEmail);
+                driver.findElement(By.xpath("//input[@id='email_create']")).sendKeys(saveEmail);
+                driver.findElement(By.xpath("//form[@id='create-account_form']//span[1]")).click();
+            }
+        }
+       System.out.println("Used email: " + saveEmail);*/
+
+          //  if (result) {
+
+           // }
 
 
 
@@ -104,4 +143,4 @@ public class Registration {
 
 
 
-}
+
