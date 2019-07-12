f3932ea484e9bf4cc0c2e2368bfb9df2e16ca2ec
Evgeny Tkach <1eluzive1@gmail.com>
2019-07-12 16:08:39 +0400

    change

--- src/test/java/Automation_Best_Practices_and_Patterns/Main.java
+++ src/test/java/Automation_Best_Practices_and_Patterns/Main.java
@@ -3,18 +3,22 @@ package Automation_Best_Practices_and_Patterns;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
+import java.util.logging.Logger;
+
 public class Main {
     @AfterTest
-    public void afterTest(){
+    public void afterMethod(){
         Page.quitDriver();
     }
 
     @Test(dataProvider = "dataProviderNewUser", dataProviderClass = DataProviderNewUser.class)
     public void RegistrationTest(NewUser newUser) {
+        Logger logger = Logger.getLogger(String.valueOf(MyLogger.class));
         Page homepage = new Homepage();
         homepage.registration(newUser);
         homepage.signIn(newUser.getUsedEmail(), newUser.getPassword());
         homepage.signOut();
+
     }
 
 
